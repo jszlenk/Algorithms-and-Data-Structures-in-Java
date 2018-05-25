d5ad9db143db0866fd8f56f05131904513039219
jszlenk <jakubszlenk@fastmail.com>
2018-05-25 22:16:39 +0200

    QuickSort

---
+++ SortAlgorithms/src/QuickSort/Main.java
@@ -0,0 +1,17 @@
+package QuickSort;
+
+import static QuickSort.QuickSort.quickSort;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = {25, 0, -8, -15, -4, 22, 7, 7, 1, -22, 14, 1};
+
+        quickSort(intArray, 0, intArray.length);
+
+        for (int anIntArray : intArray) {
+            System.out.println(anIntArray);
+        }
+    }
+}
+
---
+++ SortAlgorithms/src/QuickSort/QuickSort.java
@@ -0,0 +1,38 @@
+package QuickSort;
+
+class QuickSort {
+    static void quickSort(int[] array, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+
+        int pivotIndex = partition(array, start, end);
+        quickSort(array, start, pivotIndex);
+        quickSort(array, pivotIndex + 1, end);
+    }
+
+    private static int partition(int[] array, int start, int end) {
+        int pivot = array[start];
+        int index = start;
+        int j = end;
+
+        while (index < j) {
+
+            while (index < j && array[--j] >= pivot) ;
+            if (index < j) {
+                array[index] = array[j];
+            }
+
+            while (index < j && array[++index] <= pivot) ;
+            if (index < j) {
+                array[j] = array[index];
+            }
+
+        }
+
+        array[j] = pivot;
+        return j;
+
+    }
+}
+
