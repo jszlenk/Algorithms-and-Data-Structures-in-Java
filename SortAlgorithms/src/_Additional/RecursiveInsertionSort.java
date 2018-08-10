0f7d15837e55f295720e1231d75fa12fbc9bc094
jszlenk <jakubszlenk@fastmail.com>
2018-08-10 20:56:39 +0200

    Recursive Insertion Sort

--- SortAlgorithms/src/_Additional/RecursiveInsertionSort.java
+++ SortAlgorithms/src/_Additional/RecursiveInsertionSort.java
@@ -28,10 +28,5 @@ public class RecursiveInsertionSort {
         }
 
         input[i] = newElement;
-
-        for (i = 0; i < input.length; i++) {
-            System.out.print(input[i]);
-            System.out.print(", ");
-        }
     }
 }
