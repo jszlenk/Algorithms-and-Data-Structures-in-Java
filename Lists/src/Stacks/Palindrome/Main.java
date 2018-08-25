6ed97ddb0e5e5b09d2c6df291e050ad8a5a4cf98
jszlenk <jakubszlenk@fastmail.com>
2018-08-25 12:59:25 +0200

    Palindrome

---
+++ Lists/src/Stacks/Palindrome/Main.java
@@ -0,0 +1,9 @@
+package Stacks.Palindrome;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Palindrome.checkForPalindrome("Hello"));
+        System.out.println(Palindrome.checkForPalindrome("Don't nod"));
+    }
+}
---
+++ Lists/src/Stacks/Palindrome/Palindrome.java
@@ -0,0 +1,27 @@
+package Stacks.Palindrome;
+
+import java.util.LinkedList;
+
+class Palindrome {
+    static boolean checkForPalindrome(String string) {
+
+        LinkedList<Character> stack = new LinkedList<>();
+        StringBuilder stringNoPunctuation = new StringBuilder(string.length());
+        String lowerCase = string.toLowerCase();
+
+        for (int i = 0; i < lowerCase.length(); i++) {
+            char c = lowerCase.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                stringNoPunctuation.append(c);
+                stack.push(c);
+            }
+        }
+
+        StringBuilder reversedString = new StringBuilder(stack.size());
+        while (!stack.isEmpty()) {
+            reversedString.append(stack.pop());
+        }
+
+        return reversedString.toString().equals(stringNoPunctuation.toString());
+    }
+}
